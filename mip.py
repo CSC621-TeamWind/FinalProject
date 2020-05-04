9dbb1943ab7729c7aff35048840d40cf56a0f42a
Micaella Morales <morales.micaella@gmail.com>
2020-05-03 20:33:48 -0700

    added 3d view

--- mip.py
+++ mip.py
@@ -2,17 +2,19 @@ import PySimpleGUI as sg
 from PIL import Image
 import io
 
-import pydicom as dicom
-import matplotlib.pyplot as plt
+import pyvista as pv
 import os
 
 import vtk
+from DicomSeriesReader import dicom_series_reader
+from Medical4 import view_3d
 
 sg.theme('BrownBlue')  
 
 folder_path = ''
 files_path = []
 num_of_files = 0
+nrrd_file = './bin/ct_lungs.nrrd' 
 import_success = False
 
 def get_files():
@@ -24,10 +26,14 @@ def get_files():
     return len(files_path)
 
 def view_dcm(img_title, image_path):
-    ds = dicom.dcmread(image_path)
-    plt.title(img_title)
-    plt.imshow(ds.pixel_array)
-    plt.show()
+    # read using vtk
+    reader = vtk.vtkDICOMImageReader()
+    reader.SetFileName(image_path)
+    reader.Update()
+    data = pv.wrap(reader.GetOutput())
+    # and plot it with a bone colormap
+    data.plot(cmap='bone', cpos='xy')
+    
 
 button_prop = {'size':(8,1), 'font':('Helevetica', 12)}
 # All the elements inside the popup window. 
@@ -49,16 +55,18 @@ while True:
         folder_path = values['folder_path']
         if folder_path != '':
             num_of_files = get_files()
+            dicom_series_reader(folder_path, nrrd_file)
             import_success = True
             break
 popup.close()
 
 if import_success:
     slider_prop = {'range':(0, num_of_files), 'orientation':'h', 'size':(70, 20), 'default_value':0}
-    window_prop = {'margins':(0,0), 'size':(520, 350), 'return_keyboard_events':True}
+    window_prop = {'margins':(0,0), 'size':(520, 400), 'return_keyboard_events':True}
     buttons_prop = {'size':(30, 1), 'font':('Helvetica', 12)}
 
     col1 = [[sg.Button('View Original Image', **buttons_prop)],
+            [sg.Button('View in 3D', key='original_3d', **buttons_prop)],
             [sg.Text('_'  * 50, size=(40, 1))],
             [sg.Text('Segmentation:', size=(20,1), font=('Helvetica', 14))],
             [sg.Radio('Boundary Extraction', 'loss', **buttons_prop)],
@@ -67,6 +75,7 @@ if import_success:
             [sg.Button('Run Segmentation', **buttons_prop)]]
     
     col2 = [[sg.Button('View Transformed Image', **buttons_prop)],
+            [sg.Button('View in 3D', key="transformed_3d", **buttons_prop)],
             [sg.Text('_'  * 50, size=(40, 1))],
             [sg.Text('Quantification:', size=(20,1), font=('Helvetica', 14))],
             [sg.Radio('Quantification method1', 'loss', **buttons_prop)],
@@ -95,4 +104,6 @@ if import_success:
         if event in ('View Transformed Image'):
             index = int(values['image_slider'])
             view_dcm('Transformed Image', files_path[index]) 
+        if event in ('original_3d'):
+            view_3d(nrrd_file)
     window.close()
